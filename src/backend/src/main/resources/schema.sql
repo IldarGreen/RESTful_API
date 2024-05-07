9197accc35983384181267333487be1decfe6067
IldarGreen <igoonegreen@gmail.com>
2024-05-07 11:32:05 +0700

    add schema.sql

--- src/backend/src/main/resources/schema.sql
+++ src/backend/src/main/resources/schema.sql
@@ -32,30 +32,16 @@ CREATE TABLE IF NOT EXISTS product (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(100),
     category VARCHAR(100),
-    price
-    available_stock // число закупленных экземпляров товара
-    last_update_date // число последней закупки
-    supplier_id
-    image_id: UUID
+    price NUMERIC,
+    available_stock INT,
+    last_update_date DATE,
+    supplier_id UUID,
+    image_id: UUID,
+    FOREIGN KEY (supplier_id) REFERENCES supplier(id),
+    FOREIGN KEY (image_id) REFERENCES image(id)
 );
 
-
---CREATE TABLE IF NOT EXISTS chat.users (
---    id SERIAL PRIMARY KEY,
---    login VARCHAR(20) NOT NULL,
---    password VARCHAR(30) NOT NULL
---);
---
---CREATE TABLE IF NOT EXISTS chat.chatrooms (
---    id SERIAL PRIMARY KEY,
---    name VARCHAR(20) NOT NULL,
---    owner_id INTEGER REFERENCES chat.users(id) NOT NULL
---);
---
---CREATE TABLE IF NOT EXISTS chat.messages (
---    id SERIAL PRIMARY KEY,
---    author_id INTEGER REFERENCES chat.users(id) NOT NULL,
---    room_id INTEGER REFERENCES chat.chatrooms(id) NOT NULL,
---    text VARCHAR(1024) NOT NULL,
---    datetime TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
---);
+CREATE TABLE IF NOT EXISTS images (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    image BYTEA
+);
