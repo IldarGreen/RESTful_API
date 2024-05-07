4f84691cfa7f5ee58257155e1336f053f509ff43
IldarGreen <igoonegreen@gmail.com>
2024-05-07 12:55:06 +0700

    fix db_init file

--- src/backend/src/main/resources/init_schema.sql
+++ src/backend/src/main/resources/init_schema.sql
@@ -2,14 +2,14 @@ DROP SCHEMA IF EXISTS shop_b2 CASCADE;
 
 CREATE SCHEMA IF NOT EXISTS shop_b2;
 
-CREATE TABLE IF NOT EXISTS address (
+CREATE TABLE IF NOT EXISTS shop_b2.address (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     country VARCHAR(100),
     city VARCHAR(100),
     street VARCHAR(100)
 );
 
-CREATE TABLE IF NOT EXISTS client (
+CREATE TABLE IF NOT EXISTS shop_b2.client (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     client_name VARCHAR(100),
     client_surname VARCHAR(100),
@@ -17,18 +17,23 @@ CREATE TABLE IF NOT EXISTS client (
     gender VARCHAR(100),
     registration_date DATE,
     address_id UUID,
-    FOREIGN KEY (address_id) REFERENCES address(id)
+    FOREIGN KEY (address_id) REFERENCES shop_b2.address(id)
 );
 
-CREATE TABLE IF NOT EXISTS supplier (
+CREATE TABLE IF NOT EXISTS shop_b2.supplier (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(100),
     address_id UUID,
     phone_number VARCHAR(100),
-    FOREIGN KEY (address_id) REFERENCES address(id)
+    FOREIGN KEY (address_id) REFERENCES shop_b2.address(id)
 );
 
-CREATE TABLE IF NOT EXISTS product (
+CREATE TABLE IF NOT EXISTS shop_b2.images (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    image BYTEA
+);
+
+CREATE TABLE IF NOT EXISTS shop_b2.product (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(100),
     category VARCHAR(100),
@@ -36,12 +41,9 @@ CREATE TABLE IF NOT EXISTS product (
     available_stock INT,
     last_update_date DATE,
     supplier_id UUID,
-    image_id: UUID,
-    FOREIGN KEY (supplier_id) REFERENCES supplier(id),
-    FOREIGN KEY (image_id) REFERENCES image(id)
+    image_id UUID,
+    FOREIGN KEY (supplier_id) REFERENCES shop_b2.supplier(id),
+    FOREIGN KEY (image_id) REFERENCES shop_b2.images(id)
 );
 
-CREATE TABLE IF NOT EXISTS images (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    image BYTEA
-);
+
